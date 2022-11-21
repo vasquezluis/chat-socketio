e000cba2657579b8d47a76543b5467a72c137cfd
LuiiVasquez <luivasque95@gmail.com>
2022-11-20 21:09:42 -0600

    socket.io events

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,43 @@
 import "./App.css";
-import io from 'socket.io-client'
+import io from "socket.io-client";
+import { useState, useEffect } from "react";
 
 // pasar direccion de websockets
-const socket = io('http://localhost:4000') // escuchar eventos y enviar eventos
+const socket = io("http://localhost:4000"); // escuchar eventos y enviar eventos
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // enviar al backend - nombre valor
+    socket.emit("message", message);
+
+    setMessage("");
+  };
+
+  // useeffect para escuchar los eventos desde que carga la aplicacion
+  useEffect(() => {
+    const reciveMessage = (message) => {
+      console.log(message);
+    };
+    socket.on("message", reciveMessage);
+
+    return () => {
+      socket.off("message", reciveMessage);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+        />
+        <button>Send</button>
+      </form>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -4,24 +4,30 @@ import { Server as SocketServer } from "socket.io";
 import http from "http";
 import cors from "cors";
 
-import {PORT} from './config.js'
+import { PORT } from "./config.js";
 
 const app = express(); // app de express
 const server = http.createServer(app); // servidor de http compatible con socketio
 const io = new SocketServer(server, {
   cors: {
-    origin: '*'
-  }
+    origin: "*",
+  },
 }); // servidor con socketio
 
 app.use(cors()); // configuracion de cors - para conflictos con servidores
 app.use(morgan("dev"));
 
 // escuchar eventos con io
-io.on('connection', (socket) => {
-  console.log(socket.id);
-  console.log('Un usuario conectado');
-})
+io.on("connection", (socket) => {
+  console.log(`id: ${socket.id}`);
+
+  // escuchar el evento message del front-end | nombre valor
+  socket.on("message", (message) => {
+    console.log(message);
+    // reenviar el valor capturado al front-end | nombre valor
+    socket.broadcast.emit("message", message);
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("Hola mundo");
