446fc1c5352414cbf358711f4b93b053543c5a4e
LuiiVasquez <luivasque95@gmail.com>
2022-11-20 19:16:50 -0600

    Configuring socket.io and cors

--- server/index.js
+++ server/index.js
@@ -8,15 +8,25 @@ import {PORT} from './config.js'
 
 const app = express(); // app de express
 const server = http.createServer(app); // servidor de http compatible con socketio
-const io = new SocketServer(server); // servidor con socketio
+const io = new SocketServer(server, {
+  cors: {
+    origin: '*'
+  }
+}); // servidor con socketio
 
 app.use(cors()); // configuracion de cors - para conflictos con servidores
 app.use(morgan("dev"));
+
+// escuchar eventos con io
+io.on('connection', (socket) => {
+  console.log(socket.id);
+  console.log('Un usuario conectado');
+})
 
 app.get("/", (req, res) => {
   res.send("Hola mundo");
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto: ${PORT}`);
 });
