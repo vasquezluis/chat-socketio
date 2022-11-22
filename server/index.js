4a0fb07272597f88c021cab82759bf35d6be1203
LuiiVasquez <luivasque95@gmail.com>
2022-11-21 21:45:21 -0600

    Adding static client files to server

--- server/index.js
+++ server/index.js
@@ -3,10 +3,14 @@ import morgan from "morgan";
 import { Server as SocketServer } from "socket.io";
 import http from "http";
 import cors from "cors";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
 import { PORT } from "./config.js";
 
 const app = express(); // app de express
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log(__dirname);
 const server = http.createServer(app); // servidor de http compatible con socketio
 const io = new SocketServer(server, {
   cors: {
@@ -31,9 +35,7 @@ io.on("connection", (socket) => {
   });
 });
 
-app.get("/", (req, res) => {
-  res.send("Hola mundo");
-});
+app.use(express.static(join(__dirname, '../client/build')));
 
 server.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto: ${PORT}`);
